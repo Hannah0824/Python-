0355ebe456177236ed3fde462617b7bc90255347
Hannah0824 <124479180+Hannah0824@users.noreply.github.com>
2023-04-01 18:51:47 -0700

    Add files via upload

---
+++ Python/Week1/2_Operator.py
@@ -0,0 +1,20 @@
+# Operator 
+
+z = 0.2241
+pi = 3.141592 
+# = means input operator (math equal sign is ==)
+
+# + - / * 
+print(z+pi) 
+print(z-pi) 
+print(z/pi) 
+print(z*pi) 
+
+# / / %divmod()
+print(pi // z)    # (%)--> remainder   
+print(pi % z)     # (//) --> quotient 
+print(divmod(10,3))
+
+# ** 
+print(2**4) # same as 2*2*2*2
+ 
